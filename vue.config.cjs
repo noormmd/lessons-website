d514686a3089881870392e3c7cfeaffff24ccae8
noormmd <noormamdouh004@gmail.com>
2024-11-21 19:59:12 +0000

    Made changes to vue.config.cjs file, updating repo name to reflect current

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/noormmd.github.io/'
+  publicPath: '/lessons-website/'
 });
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/lessons-website/' : '/'
+};
