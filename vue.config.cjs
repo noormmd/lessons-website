991fab4ba806d3f54c26cedcbab6ff7cbfd80fd6
noormmd <noormamdouh004@gmail.com>
2024-11-23 22:31:55 +0000

    Changes made to index.html to mount it with app.vue

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,3 +1,8 @@
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/lessons-website/'
+});
 module.exports = {
-  publicPath: '/lessons-website/', // Setting the sub-path of my GitHub Pages URL
+  publicPath: process.env.NODE_ENV === 'production' ? '/lessons-website/' : '/'
 };
