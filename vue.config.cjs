633390b06bd8c35e8cf16818c5cb3ddcbb9a667b
noormmd <noormamdouh004@gmail.com>
2024-11-23 22:40:18 +0000

    Fixing bugs

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,8 +1,3 @@
-const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/lessons-website/'
-});
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/lessons-website/' : '/'
+  publicPath: '/lessons-website/',
 };
